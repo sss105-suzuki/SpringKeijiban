20bc09ff6b45c87ae81f942659819616b81fbe63
suzuki <suzuki@soramasu.co.jp>
2015-07-21 11:47:24 +0900

    7/17までの掲示板の状況

--- SpKeijiban/context/login.jsp
+++ SpKeijiban/context/login.jsp
@@ -6,6 +6,7 @@
 <%@ taglib prefix="x" uri="http://java.sun.com/jsp/jstl/xml"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <%@ taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="ja">
@@ -29,26 +30,27 @@
 		<div><c:out value="${errorMessage}" escapeXml="false"/></div>
 	</c:if>
 	</div>
-	<form name="loginForm" action="/SpKeijiban/login" method="post">
+	<form:form method="POST" action="/SpKeijiban/login">
 		<div class="table"
 			align="center">
 			<table border=1 class="small">
 				<tr>
 					<th class="gray">ログインID</th>
-					<td><input type="text" name="userId" value="" maxlength=20 style="width:160px;"></td></tr>
+					<td><form:input path="userid" maxlength="20" size="160"/></td>
+				</tr>
 				<tr>
 					<th class="gray">パスワード</th>
-					<td><input type="text" name="userPassWord" value="" maxlength=20 
+					<td><input type="text" name="userpassword" value="" maxlength=20 
 						style="width:160px;"></td></tr>
 			</table>
 		</div>
 		<div class="button"
 			align="center">
-			<input type="submit" name="LoginButton" value="ログイン" 
-				onclick="return chkLogin(userId.value,userPassWord.value);"/>
-			<input type="submit" name="EntryButton" value="アカウント登録" 
-				onclick="return chkLogin(userId.value,userPassWord.value);"/>
+			<input type="submit" name="Loginbutton" value="ログイン" 
+				onclick="return chkLogin(userId.value,userpassword.value);"/>
+			<input type="submit" name="Entrybutton" value="アカウント登録" 
+				onclick="return chkLogin(userId.value,userpassword.value);"/>
 		</div>
-	</form>
+	</form:form>
 </body>
 </html>
--- SpKeijiban/src/jp/co/soramasu/servlet/LoginServlet.java
+++ SpKeijiban/src/jp/co/soramasu/servlet/LoginServlet.java
@@ -1,25 +1,22 @@
 package jp.co.soramasu.servlet;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Enumeration;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import jp.co.soramasu.Interface.Check;
 import jp.co.soramasu.Interface.DB_Action;
 import jp.co.soramasu.Interface.MessageList;
+import jp.co.soramasu.common.LoginObj;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/login")
 public class LoginServlet {
 //	private ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 	
@@ -45,52 +42,48 @@ public class LoginServlet {
 
 //	private MessageList requestList = new MessageListImpl();
 	//-----------------------------------------------------------------------------------
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login() {
+		return new ModelAndView("login", "command", new LoginObj());
+	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response)
-			throws ServletException, IOException{
+	@RequestMapping(value = "/newboard", method = RequestMethod.POST)
+	protected String newboard(@ModelAttribute("SpringWeb")LoginObj request,
+			ModelMap model) {
 		
-		Enumeration<String> en = request.getParameterNames();
-		//ï∂éöâªÇØñhé~èàóù
-		request.setCharacterEncoding("Windows-31J");
 		//ì¸óÕÇ≥ÇÍÇΩï∂éöÇÃéÊìæ
-		String inputID = request.getParameter("userId");
-		String inputPass = request.getParameter("userPassWord");
+		String inputID = request.getUserid();
+		String inputPass = request.getUserpassword();
 		String errorMessage = check_act.checkPassword(inputPass) + check_act.checkID(inputID);//NullPointerException
 		try {
-			while (en.hasMoreElements()) {
-				String action = en.nextElement();
-				if ("LoginButton".equals(action)) {
-					errorMessage += check_act.checkLogin(inputPass, inputID);
-					if("".equals(errorMessage)){
-						//èëÇ´çûÇ›èÓïÒÇÃéÊìæ
-						
-						ResultSet rs = login_tbl_act.getAllMessage ();
-						if (rs.next()) {
-							rs.previous();
-							requestList.setAllMessage(rs);
-						}
-						request.setAttribute("MessageData", requestList);
-						request.setAttribute("check", check_act);
-						this.getServletContext().getRequestDispatcher("/newboard.jsp")
-							.forward(request, response);
-						return;
+			if ("".equals(request.getLoginbutton())) {
+				errorMessage += check_act.checkLogin(inputPass, inputID);
+				if("".equals(errorMessage)){
+					//èëÇ´çûÇ›èÓïÒÇÃéÊìæ
+					
+					ResultSet rs = login_tbl_act.getAllMessage ();
+					if (rs.next()) {
+						rs.previous();
+						requestList.setAllMessage(rs);
 					}
-				} else if ("EntryButton".equals(action)) {
-					if ("".equals(errorMessage)){
-						errorMessage += login_tbl_act.setUser(inputID, inputPass);
-						request.setAttribute("errorMessage", errorMessage.replaceAll("\n", "<br>"));
-						this.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
-						return;
-					}
+					model.addAttribute("MessageData", requestList);
+					model.addAttribute("check", check_act);
+					return "newboard";
 				}
+			} else if("".equals(request.getEntrybutton())) {
+				if ("".equals(errorMessage)){
+					errorMessage += login_tbl_act.setUser(inputID, inputPass);
+					model.addAttribute("errorMessage", errorMessage.replaceAll("\n", "<br>"));
+					return "login";
+				}
+				
 			}
-			request.setAttribute("errorMessage", errorMessage.replaceAll("\n", "<br>"));
-			this.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+			model.addAttribute("errorMessage", errorMessage.replaceAll("\n", "<br>"));
+			return "login";
 		} catch (SQLException e) {
 			// TODO é©ìÆê∂ê¨Ç≥ÇÍÇΩ catch ÉuÉçÉbÉN
 			e.printStackTrace();
+			return "err";
 		}
 	}
 }
